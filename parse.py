e6dad599d35e09a694fc4d7051eb6ee32115be78
Ravdeep Johar <ravdeepsinghjohar@live.com>
2013-12-05 02:52:44 -0500

    add main parse

--- parse.py
+++ parse.py
@@ -5,6 +5,8 @@ count = 0
 def main():
 	global count
 	titlecsv = open('titlecsv.csv', 'wb')
+	readtitlecsv = open('titlecsv.csv', 'rb')
+	protitlecsv = open('protitlecsv.csv', 'wb')
 	os.chdir("dataset")
 
 	
@@ -15,20 +17,46 @@ def main():
 			readfile = letter+"/"+files
 			if readfile.endswith('.htm'):
 				soup = BeautifulSoup(open(readfile))
-				#storeTitle(readfile,soup)
-				storeSummary(soup)
+				storeTitle(soup,titlecsv,readfile)
+				#storeSummary(soup)
 				#count += 1
 
-
-	titlecsv.close()
+	count(readtitlecsv,protitlecsv)
+	readtitlecsv.close()
+	protitlecsv.close()
 	#print count
 
-def storeTitle(soup,csvfile):
+def storeTitle(soup,csvfile,readfile):
 
 
-	tit =  soup.title.get_text().encode('utf8')
+	tit =  soup.title.get_text().encode('utf8').replace("\n", "").replace(",", "").rsplit("-", 1)
 
-	csvfile.write(tit+"\n")
+	st = readfile
+
+	for i in range(len(tit)):
+		st = st + "," + tit[i]
+
+	csvfile.write(st+"\n")
+
+def count(csvfile, writefile):
+
+	count = 0 
+
+	for line in csvfile:
+
+		temp = line.split(",")
+		
+
+		if(len(temp) > 2):
+
+			if "Truth!" in temp[2] or  "Fiction!" in temp[2]:
+				writefile.write(line)
+				count += 1
+
+	print count
+
+	
+
 
 
 
